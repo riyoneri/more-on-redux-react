1264d4dfa5dd290292dbb89dbc072bf43463dd5a
riyoneri <lionkaneza@gmail.com>
2022-11-28 19:12:05 +0200

    Using redux tool kit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+import { authActions } from '../store/auth';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
+  let authContent = isAuth && <ul>
+    <li>
+      <a href='/'>My Products</a>
+    </li>
+    <li>
+      <a href='/'>My Sales</a>
+    </li>
+    <li>
+      <button onClick={logoutHandler}>Logout</button>
+    </li>
+  </ul>
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {authContent}
       </nav>
     </header>
   );
